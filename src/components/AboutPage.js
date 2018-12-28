2281e2a0c564b18e74352e929f472d4c4156f29d
Floyd Orr <floydcanhelp@gmail.com>
2018-12-28 09:58:04 -0600

    spelling correction

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -44,7 +44,7 @@ class AboutPage extends Component {
                 <img src={database}  alt="database icon" style={{height:'200px',
       width: '200px'}}/>               
                  <h4 className="mt-3">Apprentices</h4>
-                <p>You've been building side projects for yourself; now do it professionally. Work hard and grow with a great comapny.</p>     
+                <p>You've been building side projects for yourself; now do it professionally. Work hard and grow with a great company!</p>     
                 </div>
               </Roll>
             </Col>  
@@ -53,7 +53,7 @@ class AboutPage extends Component {
                 <img src={react} alt="react icon" style={{height:'200px',
       width: '200px'}}/>
                 <h4 className="mt-3">Companies</h4>
-              <p>Hire a cadet @ $13 - $16 per hour with monthly $1 raises. You will have a long term employee and great culture fit.</p>
+              <p>Hire a cadet @ $16 per hour with monthly $1 raises. You will have a long term employee and great culture fit.</p>
               </Roll>
             </Col>
             <Col>
