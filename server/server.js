5bbeb4f3ade3a8dc82412f9696c4f0620c1bab34
Floyd Orr <floydcanhelp@gmail.com>
2019-01-17 07:31:09 -0600

    google verify

--- server/server.js
+++ server/server.js
@@ -1,115 +1,21 @@
-//Loading evnironmental variables here
-if (process.env.NODE_ENV !== 'production') {
-	console.log('loading dev environments')
-	require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err) => { 
+  console.log('mongo db connection works');
+}).catch('err');
+
+
+app.listen(process.env.PORT, function() {
+  console.log(`Your app is listening on port ${this.address().port}`);
 }
-require('dotenv').config()
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const dbConnection = require('./db') // loads our connection to the mongo database
-const passport = require('./passport')
-const app = express()
-const User = require('./db/models/user')//comment this and post to axios works
-console.log(User);
-require('dotenv').config()
-
-const PORT = process.env.PORT || 8080 
-var cors = require('cors')
-app.use(cors())
-
-// // ===== Middleware ====
-app.use(morgan('dev'))
-app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
 )
-app.use(bodyParser.json())
-app.use(
-	session({
-		secret: process.env.APP_SECRET || 'this is the default passphrase',
-		store: new MongoStore({ mongooseConnection: dbConnection }),
-		resave: false,
-		saveUninitialized: false
-	})
-)
-
-// // ===== Passport ====
-app.use(passport.initialize())
-app.use(passport.session()) // will call the deserializeUser
-
-// // ===== testing middleware =====
-// app.use(function(req, res, next) {
-// 	console.log('===== passport user =======')
-// 	console.log(req.session)
-// 	console.log(req.user)
-// 	console.log('===== END =======')
-// 	next()
-// })
-// testing
-// app.get(
-// 	'/auth/google/callback',
-// 	(req, res, next) => {
-// 		console.log(`req.user: ${req.user}`)
-// 		console.log('======= /auth/google/callback was called! =====')
-// 		next()
-// 	},
-// 	passport.authenticate('google', { failureRedirect: '/login' }),
-// 	(req, res) => {
-// 		res.redirect('/')
-// 	}
-// )
-
-// // ==== if its production environment!
-if (process.env.NODE_ENV === 'production') {
-	const path = require('path')
-	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, '../build/static')))
-	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../build/'))
-	})
-}
-
-// /* Express app ROUTING */
-app.use('/auth', require('./auth'))
-//app.use('/', router) //??
-
-// // ====== Error handler ====
-app.use(function(err, req, res, next) {
-	console.log('====== ERROR =======')
-	console.error(err.stack)
-	res.status(500)
-})
-
-//1 connect to server
-//2 get signup to add info to database
-app.post('/signup', (req, res) => {
-	const { username, password } = req.body
-	console.log("from server req.body: ", username, password)
-	res.send(username + " backend is being built");
-	//ADD VALIDATION
-	// User.findOne({ 'local.username': username }, (err, userMatch) =>s {
-	// 	if (userMatch) {
-	// 		return res.json({
-	// 			error: `Sorry, already a user with the username: ${username}`
-	// 		})
-	// 	}
-	// 	const newUser = new User({
-	// 		'local.username': username,
-	// 		'local.password': password
-	// 	})
-	// 	newUser.save((err, savedUser) => {
-	// 		if (err) return res.json(err)
-	// 		return res.json(savedUser)
-	// 	})
-	// })
-})
-
-// ==== Starting Server =====
-app.listen( PORT,() => {
-	console.log(`App listening on PORT: ${PORT}`)
-})
