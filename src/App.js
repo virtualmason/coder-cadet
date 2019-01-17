0b52040b7eb6855f00a5f36851b7ef2c2b7ef3e0
Floyd Orr <floydcanhelp@gmail.com>
2019-01-17 08:09:08 -0600

    add plolyfill to help with SEO

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import IntroPage from './components/IntroPage.js';
 import AboutPage from './components/AboutPage.js';
 import Footer from './components/Footer.js';
 import Tabs from './components/Tabs.js'
+import 'babel-polyfill';
+
 class App extends Component {
   render() {
     return (
